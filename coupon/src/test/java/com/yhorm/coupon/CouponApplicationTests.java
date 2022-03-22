1aac7ef50bc705b3ae785129f08550acc525dc68
yhorm <yhorm-lz@outlook.com>
2022-03-22 12:57:45 +0800

    init

--- coupon/src/test/java/com/yhorm/coupon/CouponApplicationTests.java
+++ coupon/src/test/java/com/yhorm/coupon/CouponApplicationTests.java
@@ -2,6 +2,7 @@ package com.yhorm.coupon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
 class CouponApplicationTests {
